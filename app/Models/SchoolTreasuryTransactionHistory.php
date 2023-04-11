3bde8bb7ab2cac0f0277a72fa177c04918374078
alneeel00 <mohammed0063151@gmail.com>
2023-04-11 06:34:49 +0200

    commit server 11

--- app/Models/SchoolTreasuryTransactionHistory.php
+++ app/Models/SchoolTreasuryTransactionHistory.php
@@ -26,12 +26,12 @@ class SchoolTreasuryTransactionHistory extends Model
     public function genrateUrl()
     {
         $urls = [
-            'advance' => route('Employee.Advances.index', ['id' => $this->ref_id]),
-            'salries' => route('Employee.salaries.index', ['id' => $this->ref_id]),
-            'spending' => route('Employee.spending.index', ['id' => $this->ref_id]),
-            'incentives' => '',
-            'student_revenues' => "credit",
-            'transfer_revenues'  => "credit",
+             'advance' => route('School.Advances.index', ['id' => $this->ref_id]),
+            'salries' => route('School.salaries.index', ['id' => $this->ref_id]),
+            'spending' => route('School.spending.index', ['id' => $this->ref_id]),
+            'incentives' => route('School.Teachers_allowances.index', ['id' => $this->ref_id]),
+            'student_revenues' => route('school.students.revenues.index', ['type' => 'student_revenues']),
+            'transfer_revenues'  => route('school.students.revenues.index',  ['type' => 'transfer_revenues']),
             'main_treasury' => '',
         ];
         return $urls[$this->transaction_type];
